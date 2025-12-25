7aec4d81d7df2d24863a639a0336cc897a426c23
Lou Kail <84414167+Loustoc@users.noreply.github.com>
2025-12-25 13:14:26 +0100

    fix: 9999 > float("inf")

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ with open("../image.ppm", "w") as f:
             x = _x - width/2
             y = _y - height/2
             dir = funcs.CanvasToViewport(x, -y)
-            color = funcs.TraceRay(Vector3(camPos["x"], camPos["y"], camPos["z"]), dir, 1, 9999, spheres, lights)
+            color = funcs.TraceRay(Vector3(camPos["x"], camPos["y"], camPos["z"]), dir, 1, float("inf"), spheres, lights)
             f.write(f"{color.r} {color.g} {color.b} ")
         f.write("\n")
 
