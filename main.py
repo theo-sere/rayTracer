a994a3cdc1dafe8e86fda35362814373b0bd373a
Théo Séré <104022394+theo-sere@users.noreply.github.com>
2026-01-22 09:16:17 +0100

    Add GIF and video export options to renderer
    Added support for exporting rendered frames as GIF and MP4 video in main.py, including new prompts for user selection. Updated SceneLoader to support frozen/compiled environments. Added animation.gif and animation.mp4 as example outputs. requirements.txt updated to support new features.

--- main.py
+++ main.py
@@ -7,27 +7,36 @@ import os
 scene_data = SceneLoader("scene.json").data
 Scene.set(scene_data)
 
-print("GIF generation ? (y/n)")
+print("Image (1) - GIF (2) - Video (3)")
 answer = input()
 
-if answer == "n":
-    renderer = Renderer([500, 500], "image.ppm")
-else:
+def multiCapture():
     print("How many frames ?")
     nCaptures = int(input())
     print("How long (in seconds) ?")
     duree = int(input())
     frames = []
-    tx = (Scene.final_cam.x - Scene.camera.x) / (nCaptures-1)
-    ty = (Scene.final_cam.y - Scene.camera.y) / (nCaptures-1)
-    tz = (Scene.final_cam.z - Scene.camera.z) / (nCaptures-1)
+    tx = (Scene.final_cam.x - Scene.camera.x) / (nCaptures - 1)
+    ty = (Scene.final_cam.y - Scene.camera.y) / (nCaptures - 1)
+    tz = (Scene.final_cam.z - Scene.camera.z) / (nCaptures - 1)
     for i in range(nCaptures):
-        print("Generation of the image number", i+1)
+        print("Generation of the image number", i + 1)
         renderer = Renderer([500, 500], f"image{i}.ppm")
         frames.append(imageio.v3.imread(f"image{i}.ppm"))
         Scene.camera.x += tx
         Scene.camera.y += ty
         Scene.camera.z += tz
-    imageio.mimsave('animation.gif', frames, fps=nCaptures/duree)
+    return nCaptures, duree, frames
+
+if answer == "1":
+    renderer = Renderer([500, 500], "image.ppm")
+elif answer == "2":
+    nCaptures, duree, frames = multiCapture()
+    imageio.mimsave('animation.gif', frames, fps=nCaptures / duree)
+    for i in range(nCaptures):
+        os.remove(f"image{i}.ppm")
+elif answer == "3":
+    nCaptures, duree, frames = multiCapture()
+    imageio.mimsave('animation.mp4', frames, fps=nCaptures/duree)
     for i in range(nCaptures):
         os.remove(f"image{i}.ppm")
